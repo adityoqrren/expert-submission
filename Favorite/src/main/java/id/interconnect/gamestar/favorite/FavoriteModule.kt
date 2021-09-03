04b54e5866416b7e32ab900c69c07293cb56443b
adityoqrren <aji.adityo8@gmail.com>
2021-09-04 00:27:43 +0700

    revision again

---
+++ Favorite/src/main/java/id/interconnect/gamestar/favorite/FavoriteModule.kt
@@ -0,0 +1,9 @@
+package id.interconnect.gamestar.favorite
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+
+val favoriteModule = module{
+    viewModel{FavoriteViewModel(get())}
+}
